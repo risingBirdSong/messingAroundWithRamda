0723e2275cc6a04cee712f5c7a72c4078c91acd2
risingbirdsong <heregoingthere@outlook.com>
2020-02-17 14:47:14 -0800

    now it's curried, going from 2 arguments to one, now you pass the callback fn to it in the map call

--- ramdatutjamesmoore.js
+++ ramdatutjamesmoore.js
@@ -7,12 +7,14 @@ const KelvinToFarenheit = (k) => k * 9 / 5 - 459.67;
 
 
 //by passing in convertFn, this is more generic, we can do the conversion using whichever method we like...
-const updateTemperature = (convertFn, c) => {
-  const temp = Math.round(convertFn(c.temp))
-  return R.merge(c, { temp })
+const updateTemperature = (convertFn) => {
+  return city => {
+    const temp = Math.round(convertFn(city.temp))
+    return R.merge(city, { temp })
+  }
 }
 
 //but how to pass the fn? 
-const updatedCities = cities.map(updateCity);
+const updatedCities = cities.map(updateTemperature(KelvinToFarenheit));
 
 console.log(updatedCities);
